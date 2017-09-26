710797e238aeb5365ea7631a8d3c18b636cdd55f
Jon Sanders <jonsanders101@aol.com>
2017-09-26 16:40:36 +0100

    Completed challenge 8

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,6 +1,9 @@
 require 'DockingStation'
 describe DockingStation do
-
-
-
+# #Multi-line version
+#it "Responds releasebike method" do
+  # test_dock = DockingStation.new
+  # expect(test_dock).to respond_to(:release_bike)
+#  subject {DockingStation.new}
+  it {is_expected.to respond_to :release_bike}
 end
