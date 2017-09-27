e6b85af011f22181d749b27860ce03539e885af5
Jon Sanders <jonsanders101@aol.com>
2017-09-27 15:00:31 +0100

    Created dock method

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,7 +1,15 @@
 require_relative 'Bike'
 
+
 class DockingStation
+
+  attr_reader :docked_bike
+
   def release_bike
     Bike.new
+  end
+
+  def dock bike
+    @docked_bike = bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -9,4 +9,12 @@ describe DockingStation do
   it 'has a method release_bike that returns a working bike' do
     expect(subject.release_bike.working?).to eq true
   end
+
+  it {is_expected.to respond_to :dock}
+
+
+  it 'has a method dock that accepts a bike' do
+    subject.dock Bike.new
+    expect(subject.docked_bike).to be_instance_of Bike
+  end
 end
