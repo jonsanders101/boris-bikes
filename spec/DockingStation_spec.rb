485a66bd4a20f75fa8d1ebf7bff4fcb0e4e7d9dc
Ed Lowther <ed.lowther[at]gmail.com>
2017-09-27 12:49:34 +0100

    Made docking stations release bikes

--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,9 +1,12 @@
 require 'DockingStation'
+require 'Bike'
+
 describe DockingStation do
-# #Multi-line version
-#it "Responds releasebike method" do
-  # test_dock = DockingStation.new
-  # expect(test_dock).to respond_to(:release_bike)
-#  subject {DockingStation.new}
   it {is_expected.to respond_to :release_bike}
+  it 'has a method release_bike that returns a bike' do
+    expect(subject.release_bike).to be_instance_of Bike
+  end
+  it 'has a method release_bike that returns a working bike' do
+    expect(subject.release_bike.working?).to eq true
+  end
 end
