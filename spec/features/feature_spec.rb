19fb125d45bbbced4b04d298c35052040c8f9028
Jon Sanders <jonsanders101@aol.com>
2017-10-12 19:53:30 +0100

    Added GEMFILE

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -2,13 +2,11 @@ require 'docking_station'
 require 'bike'
 
 describe 'loads MAX_CAPACITY bikes then tests one more' do
+
   it 'should raise an error' do
     docking_station_a = DockingStation.new
-    bikes = []
-    DockingStation::MAX_CAPACITY.times { |_loop| bikes << Bike.new }
-    bikes.each do |bike|
-      docking_station_a.dock bike
-    end
+    DockingStation::MAX_CAPACITY.times { docking_station_a.dock Bike.new }
     expect { docking_station_a.dock Bike.new }.to raise_error('Docking station full')
   end
+
 end
